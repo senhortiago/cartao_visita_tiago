78b826598d04d276c990e165f141722a7732cec8
Tiago Leal <98552514+senhortiago@users.noreply.github.com>
2022-02-02 09:50:25 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ function changeSocialMidea(){
     for (let li of socialLinks.children){
         const social = li.getAttribute('class')
         li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+        /*
+        Não entendi muito bem esse for, pq ele não pega o link do github, começa a pegar apenas do instagram,
+        eu sei que essa era a intenção mas não entendi o pq ele não pega de lá.
+        */
     }  
 }
 changeSocialMidea()
